4b77d29a47e3ae6dbd9912e4ecb05cab4248769a
Oliver Park <oliverco0828@gmail.com>
2024-08-15 13:02:20 +0900

    Initial Commit

---
+++ SwiftConcurrencyBootCamp/SwiftConcurrencyBootCampApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftConcurrencyBootCampApp.swift
+//  SwiftConcurrencyBootCamp
+//
+//  Created by Oliver Park on 8/15/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftConcurrencyBootCampApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
